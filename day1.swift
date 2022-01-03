66b25054e789992807f042f057b6f185a58791c6
Seton Hawkins <setononlinestuff@gmail.com>
2022-01-03 14:05:43 -0500

    Implemented Day 1
    First attempt
    Limited crying

--- day1.swift
+++ day1.swift
@@ -1,1 +1,46 @@
-print("Hello, World!")
+import Foundation
+import SwiftUI
+
+// Determine the file name
+let filename = "/Users/setonhawkins/dev/advent-of-code-2015/day-1-input.txt"
+
+// Read the contents of the specified file
+let contents = try! String(contentsOfFile: filename)
+
+// Print out file contents
+// print(contents)
+
+//Variable Count?
+var part1Count = 0
+
+for character in contents {
+  //print(character)
+  if character == "(" {
+    part1Count = part1Count + 1
+  }
+  if character == ")" {
+    part1Count = part1Count - 1
+  }
+}
+
+print(part1Count)
+
+//Variable Count?
+var Floor = 0
+var Part2Count = 0
+
+for character in contents {
+  if Floor < 0 {
+    break
+  }
+
+  Part2Count += 1
+  if character == "(" {
+    Floor += 1
+  }
+  if character == ")" {
+    Floor -= 1
+  }
+}
+
+print(Part2Count)
